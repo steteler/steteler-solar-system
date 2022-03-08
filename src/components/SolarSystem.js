53f9dcf2bc80712b8a32f3dae184670b2ae5b087
kikutii <joaovictorkikuti@gmail.com>
2022-03-07 21:30:48 -0300

    estilizado classe planetsContainer e planets

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -2,23 +2,26 @@ import React from 'react';
 import Title from './Title';
 import PlanetCard from './PlanetCard';
 import planets from '../data/planets';
+import '../css/SolarSystem.css';
 
 class SolarSystem extends React.Component {
   render() {
     return (
-      <div data-testid="solar-system">
+      <div className="planetsContainer" data-testid="solar-system">
         <Title headline="Planetas" />
-        {
-          planets.map(
-            (planet, index) => (
-              <PlanetCard
-                key={ index }
-                planetName={ planet.name }
-                planetImage={ planet.image }
-              />
-            ),
-          )
-        }
+        <div className="planets">
+          {
+            planets.map(
+              (planet, index) => (
+                <PlanetCard
+                  key={ index }
+                  planetName={ planet.name }
+                  planetImage={ planet.image }
+                />
+              ),
+            )
+          }
+        </div>
       </div>
     );
   }
